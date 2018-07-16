75a697e2d9ee637f7cba29ef24fcf67b7bd31a5a
mauriciog98 <33387474+mauriciog98@users.noreply.github.com>
2018-07-16 06:07:13 -0500

    Update bot.js (#50)
    Chat title index has change

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@
 	var elementConfig = {
 		"chats": [1, 0, 5, 2, 0, 3, 0, 0, 0],
 		"chat_icons": [0, 0, 1, 1, 1, 0],
-		"chat_title": [0, 0, 1, 0, 0, 0],
+		"chat_title": [0, 0, 1, 0, 0, 0, 0],
 		"chat_lastmsg": [0, 0, 1, 1, 0, 0],
 		"chat_active": [0, 0],
 		"selected_title": [1, 0, 5, 3, 0, 1, 1, 0, 0, 0, 0]
